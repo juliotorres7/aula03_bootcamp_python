85622d1a78e8e5c727f045ce5efaaf5c8a9c73d0
Julio Torres <julio.torres7@outlook.com>
2025-07-16 17:37:00 -0300

    comentando codigo para testar

--- main.py
+++ main.py
@@ -66,12 +66,12 @@
 # a R$ 10.000 ou se ocorrer fora do horário comercial (antes das 9h ou depois das 18h). 
 # Dada uma transação como `transacao = {'valor': 12000, 'hora': 20}`, verifique se ela é suspeita.
 
-transacao = {'valor': 12000, 'hora': 20}
+# transacao = {'valor': 12000, 'hora': 20}
 
-if transacao['valor']>10000 or (transacao["hora"]<9 and transacao["hora"]>20):
-    print("transacao suspeita")
-else:
-    print("transacao ok")
+# if transacao['valor']>10000 or (transacao["hora"]<9 and transacao["hora"]>20):
+#     print("transacao suspeita")
+# else:
+#     print("transacao ok")
 
 ### Exercício 6. Contagem de Palavras em Textos
 # Objetivo:** Dado um texto, contar quantas vezes cada palavra única aparece nele.
